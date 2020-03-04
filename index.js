21ea961d340fdbc5e976aad6805326661d3bd0d5
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2020-03-04 14:54:03 -0300

    projeto listando e deletando

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ var app = express()
 var bodyparser = require('body-parser')
 var Aluno = require('./model/aluno')
 
-
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: false }))
 app.set('view engine','ejs')
 
-app.get('/',function(req,res){
-    res.render('listar.ejs',{})
-})
 
+//ROUTES
+//create get (OK)
 app.get('/add',function(req,res){
-    res.render('adicionar.ejs',{})
+    res.render('adicionar.ejs',{msg:''})
 })
 
+//create post (OK)
 app.post('/add',function(req,res){
     var aluno = new Aluno({
         nome: req.body.nome,
@@ -23,30 +22,45 @@ app.post('/add',function(req,res){
         telefone: req.body.telefone
     })
     aluno.save(function(err){
-        Aluno.find({}).exec(function(e,docs){
-            if(err){
-                res.render('listar.ejs',{ msg: err, listaAlunos: docs })
-            }else{
-                res.render('listar.ejs',{ msg: "Salvo com sucesso!", listaAlunos: docs })
-            }
-        });       
+        if(err){
+            res.render("adicionar.ejs",{msg: err})
+        }else{
+            res.render('adicionar.ejs',{msg: "Adicionado com sucesso!"})
+        }
     })
 })
 
-app.get('/edit',function(req,res){
+//read get (OK)
+app.get('/',function(req,res){
+    Aluno.find({}).exec(function(err,docs){
+        res.render('listar.ejs',{ listaAlunos: docs, msg:"" })
+    })    
+})
+
+//read post
+app.post('/',function(req,res){
+    res.render('listar.ejs',{})
+})
+
+//update get/
+app.get('/edit/:id',function(req,res){
     res.render('editar.ejs',{})
 })
 
+//update post
+app.post('/edit/:id',function(req,res){
+    res.render('editar.ejs',{})
+})
+
+//delete get
 app.get('/del/:id',function(req,res){
-    console.log(req.params.id)
     Aluno.findByIdAndDelete(req.params.id,function(err){
         if(err){
-            res.send(err)
+            res.redirect('/')
         }else{
-            res.send("Tudo ok")
+            res.redirect('/')
         }
     });
-    //res.redirect('/');
 })
 
 app.listen(3000,function(){
